77bb91473ca86cab15c4ec2461cb8b959849468c
nhlee <noirhero83@gmail.com>
2019-12-30 02:47:59 +0900

    랜더링 시스템 작업 중.

--- system/render_sprite_system.js
+++ system/render_sprite_system.js
@@ -3,7 +3,9 @@
 const RenderSpriteSystem = CES.System.extend({
   init: function() {
     this.program = new RenderProgram("../assets/sprite.vert", "../assets/sprite.frag");
+
     this.world_tm = glMatrix.mat4.create();
+    this.worls_pos = glMatrix.vec3.create();
   },
   update: function() {
     if(false === this.program.UseProgram()) {
@@ -18,6 +20,7 @@ const RenderSpriteSystem = CES.System.extend({
     GL.blendFuncSeparate(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA, GL.ONE, GL.ONE_MINUS_SRC_ALPHA);
 
     const world_tm = this.world_tm;
+    const world_pos = this.world_pos;
     this.world.getEntities("Pos", "Scale").forEach(entity => {
       const rot_comp  = entity.getComponent("Rot");
       const rot = rot_comp ? rot_comp.rot : INIT_QUAT;
@@ -25,6 +28,18 @@ const RenderSpriteSystem = CES.System.extend({
       const scale = entity.getComponent("Scale").scale;
 
       glMatrix.mat4.fromRotationTranslationScale(world_tm, rot, pos, scale);
+
+      for(let i = 0; i < 4; ++i) {
+        glMatrix.vec3.transformMat4(world_pos, INIT_POS[i], world_tm);
+
+        XYZUVI_VB[offset++] = world_pos[0];
+        XYZUVI_VB[offset++] = world_pos[1];
+        //XYZUVI_VB[offset++] = (0 === pos[2]) ? world_transform[13] : pos[2];
+        XYZUVI_VB[offset++] = pos[2];
+        XYZUVI_VB[offset++] = texcoord[i][0];
+        XYZUVI_VB[offset++] = texcoord[i][1];
+        XYZUVI_VB[offset++] = texture_index;
+      }
     });
   },
 });
